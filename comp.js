b4dda8470d7cb38285f8ccde7a428929c8bbf351
Shrey19702 <90191696+Shrey19702@users.noreply.github.com>
2021-10-07 22:52:35 +0530

    Create comp.js

---
+++ comp.js
@@ -0,0 +1,15 @@
+exports.add= function add(x,y){
+    return x+y;
+}
+exports.sub= function add(x,y){
+    return x-y;
+}
+exports.mul= function add(x,y){
+    return x*y;
+}
+exports.div= function add(x,y){
+    return x/y;
+}
+
+//modules.exports==exports
+// the functions are added to an module object to be exported same can be done for arrays, data
