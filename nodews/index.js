3fc5d3bea0c8802f24622c93429244538f5cf700
Shrey19702 <90191696+Shrey19702@users.noreply.github.com>
2021-10-10 20:45:38 +0530

    Update index.js

--- nodews/index.js
+++ nodews/index.js
@@ -1,12 +1,13 @@
-const http= require('http');
+const http= require('http'); // module http 
 const port= 8000;
-const fs= require('fs');
+const fs= require('fs'); //for readiing and writting in / from files
 
-function requestHandler(req, res){
-    console.log(req.url);
-    res.writeHead(200, {'content-type': 'text/html'});
+function requestHandler(req, res){    // function handles request givin to server
+    
+    console.log(req.url);  // req.url gives the url requested by  client  ex.->  /profile
+    res.writeHead(200, {'content-type':'text/html'}); // 200 is number tag for sucesssful, 404 for errors.... object contains data for output  
 
-    let pageloc;
+    let pageloc;  //we will location of the required page here
     switch(req.url){
         case "/" :
             pageloc= 'index.html';
@@ -18,24 +19,29 @@ function requestHandler(req, res){
             pageloc= 'error.html'
     }
 
-    fs.readFile(pageloc, function(error, data){
+    fs.readFile(pageloc, function(error, data){      // reading the file index.html (error for an error in reading)
         if(error){
-            return res.end('<h1>no page location found</h1>');
+            return res.end('<h1>no page location found</h1>');  //return this html to it if wrong file 
         }
         else{
-            return res.end(data);
+            return res.end(data);  // return data containing html from the location 
         }
     });
 }
 
-const server= http.createServer(requestHandler);
+const server = http.createServer(requestHandler); //server created on machine
 
-server.listen(port, function(error){
-
+server.listen(port, function(error){   //listen to the server created 
     if(error){
         console.log(error);
     }
     else{
-        console.log("server working on port : ", port);
+        console.log('server running on port :',port);
     }
 });
+//localhost == 127.0.0.1
+// all local servers run on ==> localhost:portnumber
+// no output given so page doesn't load
+// in this case it runs on 127.0.0.1:8000
+// run the file using node command
+// use ctrl+C to end the program 
